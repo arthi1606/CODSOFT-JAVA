32398000423a9a3b989543e6b2e397f414f6e177
arthi <arthi@ArthiPriya>
2024-08-01 18:58:58 +0530

    TASK2 STUDENT GRADE CALCULATOR

---
+++ InternShipCodSoft/src/task2/StudentGradeCalculator.java
@@ -0,0 +1,48 @@
+package task2;
+
+
+import java.util.Scanner;
+public class StudentGradeCalculator {
+
+    public static char calculateGrade(double percentage) {
+        if (percentage >= 90) {
+            return 'A';
+        } else if (percentage >= 80) {
+            return 'B';
+        } else if (percentage >= 70) {
+            return 'C';
+        } else if (percentage >= 60) {
+            return 'D';
+        } else {
+            return 'F';
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter the number of subjects: ");
+        int numSubjects = scanner.nextInt();
+        double[] marks = new double[numSubjects];
+
+        for (int i = 0; i < numSubjects; i++) {
+            System.out.print("Enter the marks for subject " + (i + 1) + ": ");
+            marks[i] = scanner.nextDouble();
+        }
+
+        double totalMarks = 0;
+        for (double mark : marks) {
+            totalMarks += mark;
+        }
+
+        double averagePercentage = totalMarks / numSubjects;
+        char grade = calculateGrade(averagePercentage);
+
+        System.out.println("\nTotal Marks: " + totalMarks);
+        System.out.println("Average Percentage: " + String.format("%.2f", averagePercentage) + "%");
+        System.out.println("Grade: " + grade);
+
+        scanner.close();
+    }
+}
+
